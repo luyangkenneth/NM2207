e094a761ecf886f5ebf030cbbdef22601c874dfe
Kenneth Lu <lu.yang.kenneth@gmail.com>
2018-04-25 00:08:02 +0800

    Add keyboard event listeners

--- fpdir/game.js
+++ fpdir/game.js
@@ -1,8 +1,44 @@
+const keys = {
+  up: false,
+  left: false,
+  right: false,
+};
+
+//////////////////////////////////////////////////
+
 const run = () => {
   setup();
 };
 
 const setup = () => {
+  window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+      case 'ArrowUp':
+        keys.up = true;
+        break;
+      case 'ArrowLeft':
+        keys.left = true;
+        break;
+      case 'ArrowRight':
+        keys.right = true;
+        break;
+    }
+  });
+
+  window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+      case 'ArrowUp':
+        keys.up = false;
+        break;
+      case 'ArrowLeft':
+        keys.left = false;
+        break;
+      case 'ArrowRight':
+        keys.right = false;
+        break;
+    }
+  });
+
   const header = document.getElementById('header');
   const main = document.getElementById('main');
 
